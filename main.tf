205bfd7820ed3de7fc09a3900a0b3366ad999250
chinnapareddy <chinna.aduri@gmail.com>
2023-10-22 02:10:44 -0700

    terraform structure

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_security_group" "main" {
   name        = "${local.name_prefix}-sg"
   description = "${local.name_prefix}-sg"
   vpc_id      = var.vpc_id
-  tags = merge(local.tags, {Name = local."${local.name_prefix}-sg"})
+  tags = merge(local.tags, {Name = "${local.name_prefix}-sg"})
 
   ingress {
-    description = "DOCDB"
+    description = "docdb"
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
@@ -26,15 +26,25 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb4.0"
+  name        =  "${local.name_prefix}-sg"
+  description = "${local.name_prefix}-sg"
+  tags = merge(local.tags, {Name = "${local.name_prefix}-pg"})
+}
+
 
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  = "docdb"
-  master_username         = data.aws_ssm_parameter.master_username.value
-  master_password         = data.aws_ssm_parameter.master_password.value
-  backup_retention_period = var.backup_retention_period
-  preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = var.skip_final_snapshot
+  cluster_identifier              = "${var.env}-docdb-cluster"
+  engine                          = "docdb"
+  master_username                 = data.aws_ssm_parameter.master_username.value
+  master_password                 = data.aws_ssm_parameter.master_password.value
+  backup_retention_period         = var.backup_retention_period
+  preferred_backup_window         = var.preferred_backup_window
+  skip_final_snapshot             = var.skip_final_snapshot
+  db_subnet_group_name            = [aws_security_group.main.id]
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  tags                            = merge(local.tags, {Name = "${local.name_prefix}-cluster"})
 }
 
 
